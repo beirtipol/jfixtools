d448a62bb9ff5494e4fc861f4ae80f83d5f6e465
beirtipol <beirtipol@gmail.com>
2020-03-14 20:43:48 +0000

    Cleanup - remove unnecessary H2 config class and refactor test to be specifically for FIXMessages. Add test for retrieving by date

--- jfixtools-logging/src/main/java/com/beirtipol/jfixtools/logging/jpa/FIXEventLogRepository.java
+++ jfixtools-logging/src/main/java/com/beirtipol/jfixtools/logging/jpa/FIXEventLogRepository.java
@@ -23,6 +23,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 @Repository
