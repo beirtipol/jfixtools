37c2730e5934e688f3abd2addd97d893d758f35d
beirtipol <beirtipol@gmail.com>
2020-04-02 12:38:51 +0100

    Test events as well as message retrieval. Learn interesting things about h2-driven testing

--- jfixtools-logging/src/test/java/com/beirtipol/jfixtools/logging/jpa/FIXLogJPAServiceTest.java
+++ jfixtools-logging/src/test/java/com/beirtipol/jfixtools/logging/jpa/FIXLogJPAServiceTest.java
@@ -19,8 +19,9 @@ package com.beirtipol.jfixtools.logging.jpa;
 import com.beirtipol.jfixtools.logging.FIXLogRepository;
 import com.beirtipol.jfixtools.logging.model.FIXLogEntry;
 import com.beirtipol.jfixtools.logging.model.FIXLogEntryType;
-import com.beirtipol.jfixtools.logging.model.FIXMessagesLogEntry;
 import com.beirtipol.jfixtools.logging.model.FIXSessionID;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,7 +34,6 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @Sql({"/com/beirtipol/jfixtools/logging/jpa/data.sql"})
@@ -42,25 +42,48 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 public class FIXLogJPAServiceTest {
     @Autowired
     private FIXLogJPAService service;
+    private static FIXSessionID     sessionID;
+    private static LocalDateTime    fromDate;
+    private static LocalDateTime    toDate;
 
-
-    @Test
-    public void loadTestDataAndRetrieveFromFilteredRepository() {
-        List<FIXLogRepository> repositories = service.getFIXLogJPARepositories();
-        FIXSessionID sessionID = FIXSessionID.builder()
+    @BeforeAll
+    public static void setup() {
+        sessionID = FIXSessionID.builder()
                 .senderCompID("Acceptor")
                 .targetCompID("Initiator")
                 .beginString("FIX.4.4")
                 .build();
-        Optional<FIXLogRepository> messageRepo = repositories.stream()
-                .filter(f -> f.getLogEntryType() == FIXLogEntryType.MESSAGE)
-                .filter(f -> f.getSessionID().matches(sessionID))
-                .findFirst();
-        List<? extends FIXLogEntry> messages = messageRepo.get().getEntriesBetween(
-                LocalDateTime.of(2020, 4, 2, 9, 10, 52),
-                LocalDateTime.of(2020, 4, 2, 9, 10, 54));
+
+        fromDate = LocalDateTime.of(2020, 4, 2, 9, 10, 52);
+        toDate   = LocalDateTime.of(2020, 4, 2, 9, 10, 54);
+    }
+
+    /**
+     * The reason we put all of this in a single test is due to how the @Sql annotation is run before (or after) each test.
+     * I could (should) make a cleandown script for the SQL but this will suffice for now.
+     *
+     * @see https://stackoverflow.com/questions/47775560/how-can-springs-test-annotation-sql-behave-like-beforeclass
+     */
+    @Test
+    public void loadTestDataAndRetrieveFromFilteredRepository() {
+        Optional<FIXLogRepository> messageRepo = findRepository(FIXLogEntryType.MESSAGE);
+        List<? extends FIXLogEntry> messages = messageRepo.get().getEntriesBetween(fromDate, toDate);
 
         assertNotNull(messages);
         assertThat(messages.size()).isEqualTo(2);
+
+        Optional<FIXLogRepository> eventRepo = findRepository(FIXLogEntryType.EVENT);
+        List<? extends FIXLogEntry> events = eventRepo.get().getEntriesBetween(fromDate, toDate);
+
+        assertNotNull(events);
+        assertThat(events.size()).isEqualTo(1);
+    }
+
+    public Optional<FIXLogRepository> findRepository(FIXLogEntryType entryType) {
+        List<FIXLogRepository> repositories = service.getFIXLogJPARepositories();
+        return repositories.stream()
+                .filter(f -> f.getLogEntryType() == entryType)
+                .filter(f -> f.getSessionID().matches(sessionID))
+                .findFirst();
     }
 }
